20f00e1a81538a72c93fb4d4e330712dbcb26a29
gustavo-plc <gustavoparreira@hotmail.com>
2025-01-01 18:07:48 -0300

    importing Numpy package

---
+++ Python/packages.py
@@ -0,0 +1,9 @@
+# different ways to import packages
+import numpy as np
+from numpy import array
+
+
+lista = [1, 2, 3]
+
+np.array(lista) #this way, itś very clear the programmer used the numpy package
+array(lista)
