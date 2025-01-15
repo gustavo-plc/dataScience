8ca015bbf0b1f3fa32b80d30be1c24a4a0b2dc04
gustavo-plc <gustavoparreira@hotmail.com>
2025-01-15 17:19:28 -0300

    using dataframes as a new data structure approach

---
+++ test_pandas.py
@@ -0,0 +1,19 @@
+# USING DATAFRAMES TO COMPILE INFORMATION: A NEW DATA STRUCTURE APPROACH
+
+import pandas as pd
+
+# Pre-defined lists
+names = ['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']
+dr =  [True, False, False, False, True, True, True]
+cpc = [809, 731, 588, 18, 200, 70, 45]
+
+# Import pandas as pd
+
+
+# Create dictionary my_dict with three key:value pairs: my_dict
+
+
+# Build a DataFrame cars from my_dict: cars
+
+
+# Print cars
