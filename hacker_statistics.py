608b41fefbe8212cb43027c9d4e97d8dbcf16142
gustavo-plc <gustavoparreira@hotmail.com>
2025-01-19 18:17:38 -0300

    another example of random walk

--- hacker_statistics.py
+++ hacker_statistics.py
@@ -59,8 +59,34 @@ for x in range(10):
     tails.append(tails[x] + coin) #the result is appended to the list tails updating its last result
 
 print(tails)
-
+print()
 # THE EXAMPLES ABOVE SHOWED HOW TO
 # TRANSFORM A BUNCH OF STEPS INTO A RANDOM WALK
 
 
+#another example
+
+# Initialize random_walk
+random_walk = [0]
+
+# Complete the ___
+for x in range(100) :
+    # Set step: last element in random_walk
+    step = random_walk[-1]
+
+    # Roll the dice
+    dice = np.random.randint(1,7)
+
+    # Determine next step
+    if dice <= 2:
+        step = step - 1
+    elif dice <= 5:
+        step = step + 1
+    else:
+        step = step + np.random.randint(1,7)
+
+    # append next_step to random_walk
+    random_walk.append(step)
+
+# Print random_walk
+print(random_walk)
