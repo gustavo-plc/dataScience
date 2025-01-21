098c89c976fbaa6df8b5d7d70fe9028a12b6972e
gustavo-plc <gustavoparreira@hotmail.com>
2025-01-21 14:05:14 -0300

    adding some files to the project

--- test_pandas.py
+++ test_pandas.py
@@ -43,3 +43,11 @@ for k, v in europe.items(): #the method items() has to be used to do so
 
 # DATA MANIPULATION WITH Pandas!
 
+# When you get a new DataFrame to work with, the first thing you need to do is explore it and see what it contains.
+# There are several useful methods and attributes for this.
+#
+#     .head() returns the first few rows (the “head” of the DataFrame).
+#     .info() shows information on each of the columns, such as the data type and number of missing values.
+#     .shape returns the number of rows and columns of the DataFrame.
+#     .describe() calculates a few summary statistics for each column.
+
