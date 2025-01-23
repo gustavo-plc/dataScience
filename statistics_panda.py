f75ac9b947a4959925628d8b41596dc179de7fc7
gustavo-plc <gustavoparreira@hotmail.com>
2025-01-23 07:40:14 -0300

    table creation by doing statistics calculations on datasets

--- statistics_panda.py
+++ statistics_panda.py
@@ -253,8 +253,12 @@ print(mean_med_sales_by_type)
 print()
 
 # Pivot for mean weekly_sales by store type and holiday
-mean_sales_by_type_holiday = sales.pivot_table(values = 'weekly_sales', index = ['type', 'is_holiday'])
+mean_sales_by_type_holiday = sales.pivot_table(values = 'weekly_sales', index = 'type', columns = 'is_holiday')
 
 # Print mean_sales_by_type_holiday
 print(mean_sales_by_type_holiday)
+print()
+
+# Pivot tables are another way to do the same thing as a group-by-then-summarize.
+
 
